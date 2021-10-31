fb4c430e8d2a7a68d4889a69f6700e86d7ffd2b1
konstantysz <kostekszumigaj@gmail.com>
2021-10-31 20:02:30 +0100

    Add LinkedListWithTail

--- aisd-data-structures/LinkedList.h
+++ aisd-data-structures/LinkedList.h
@@ -3,23 +3,24 @@
 
 namespace aisd 
 {
-	template <typename T>
-	struct Node
-	{
-		T val;
-		Node<T>* next;
-
-		Node(T v, Node<T>* n = nullptr)
-		{
-			val = v;
-			next = n;
-		}
-	};
-
-	template <typename T>
+	template <class T>
 	class LinkedList
 	{
-		Node<T>* head;
+	protected:
+		template <typename T>
+		struct Node
+		{
+			T val;
+			Node<T>* next;
+
+			Node(T v, Node<T>* n = nullptr)
+			{
+				val = v;
+				next = n;
+			}
+		};
+
+		Node<T>* head = nullptr;
 
 	public:
 		void Insert(T v)
@@ -34,9 +35,58 @@ namespace aisd
 			Node<T>* p = head;
 			while (p)
 			{
-				std::cout << p->val << std::endl;
+				std::cout << p->val;
+				p = p->next;
+				if (p)
+				{
+					std::cout << " -> ";
+				}
+			}
+			std::cout << std::endl;
+		}
+
+		void InsertLast(T v)
+		{
+			Node<T>* p = head;
+			while (p->next)
+			{
 				p = p->next;
 			}
+			p->next = new Node<T>(v);
+		}
+	};
+
+	template <class T>
+	class LinkedListWithTail : public LinkedList<T>
+	{
+		typename LinkedList<T>::template Node<T>* tail = nullptr;
+
+	public:
+		void Insert(T v)
+		{
+			typename LinkedList<T>::template Node<T>* newHead = new typename LinkedList<T>::template Node<T>(v);
+			newHead->next = LinkedList<T>::head;
+			LinkedList<T>::head = newHead;
+			if (!tail)
+			{
+				tail = newHead;
+			}
+		}
+
+		void InsertLast(T v)
+		{
+			typename LinkedList<T>::template Node<T>* p = new typename LinkedList<T>::template Node<T>(v);
+
+			if (!LinkedList<T>::head)
+			{
+				LinkedList<T>::head = p;
+				tail = p;
+			}
+			else
+			{
+				tail->next = p;
+			}
+			tail = p;
 		}
 	};
 } // namespace aisd
